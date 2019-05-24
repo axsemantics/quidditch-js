f47ed221dac7d20968935c52fec780a9ef91a172
Wolfgang Fellger <wolfgang.fellger@ax-semantics.com>
2019-05-24 13:53:19 +0200

    Tabify

--- test/delta/compose.test.js
+++ test/delta/compose.test.js
@@ -284,9 +284,9 @@ describe('Delta.compose()', () => {
 	})
 
 	it('should compose empty $sub', function () {
-    const a = new Delta([{retain: 1, $sub: []}])
-    const b = new Delta([{retain: 2}, {insert: {_t: 'case', items: []}}])
-    expect(a.compose(b)).to.equalDelta(b)
+		const a = new Delta([{retain: 1, $sub: []}])
+		const b = new Delta([{retain: 2}, {insert: {_t: 'case', items: []}}])
+		expect(a.compose(b)).to.equalDelta(b)
 	})
 
 	// do we need type checks in the frontend?
